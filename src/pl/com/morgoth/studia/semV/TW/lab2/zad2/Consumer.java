9bc3c1013905f680c85baabac739bdc8dfc488aa
Morgarooth <mateuszjaje@gmail.com>
2013-10-21 11:21:33 +0200

    a

--- src/pl/com/morgoth/studia/semV/TW/lab2/zad2/Consumer.java
+++ src/pl/com/morgoth/studia/semV/TW/lab2/zad2/Consumer.java
@@ -3,7 +3,7 @@ package pl.com.morgoth.studia.semV.TW.lab2.zad2;
 public class Consumer extends Thread {
 
 	public Consumer(Element[] buffer, int maxIntermediaryThreads) {
-		// TODO Auto-generated constructor stub
+
 	}
 
 }
