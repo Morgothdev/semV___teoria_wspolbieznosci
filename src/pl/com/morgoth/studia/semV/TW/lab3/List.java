a348766c13ea34e4d7b1e0cc9b9fb00e0ac9af0b
Morgarooth <mateuszjaje@gmail.com>
2013-10-23 23:13:11 +0200

    s

---
+++ src/pl/com/morgoth/studia/semV/TW/lab3/List.java
@@ -0,0 +1,18 @@
+package pl.com.morgoth.studia.semV.TW.lab3;
+
+public class List {
+
+	private Node head;
+
+	public boolean add(Object newObject) {
+		return false;
+	}
+
+	public boolean remove(Object objectToRemove) {
+		return false;
+	}
+
+	public boolean contains(Object object) {
+		return false;
+	}
+}
