7d1876336e40c424bb901d1da773b5b48be3e6f0
Morgarooth <mateuszjaje@gmail.com>
2013-10-26 11:40:31 +0200

    gotowa lista

--- src/pl/com/morgoth/studia/semV/TW/lab3/Node.java
+++ src/pl/com/morgoth/studia/semV/TW/lab3/Node.java
@@ -4,39 +4,13 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Node {
-	private Node next;
-	private Lock lock = new ReentrantLock();
-	private Object value;
+	public Node next;
+	public final Lock lock = new ReentrantLock();
+	public Object value;
 
 	public Node(Object value, Node next) {
 		this.value = value;
 		this.next = next;
 	}
 
-	public Node getNext() {
-		return next;
-	}
-
-	public Node setNext(Node next) {
-		this.next = next;
-		return this;
-	}
-
-	public Lock getLock() {
-		return lock;
-	}
-
-	public Node setLock(Lock lock) {
-		this.lock = lock;
-		return this;
-	}
-
-	public Object getValue() {
-		return value;
-	}
-
-	public Node setValue(Object value) {
-		this.value = value;
-		return this;
-	}
 }
