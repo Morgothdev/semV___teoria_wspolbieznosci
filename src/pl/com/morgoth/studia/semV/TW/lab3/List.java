1fab82bec46cf59a1795e9be69dbf3f9a10cccd7
Morgarooth <mateuszjaje@gmail.com>
2013-10-26 11:46:56 +0200

    dodanie intefejsu listy oraz przemianowanie List na drobnoziarnista liste

--- src/pl/com/morgoth/studia/semV/TW/lab3/List.java
+++ src/pl/com/morgoth/studia/semV/TW/lab3/List.java
@@ -1,52 +1,11 @@
 package pl.com.morgoth.studia.semV.TW.lab3;
 
-public class List {
+public interface List {
 
-	private final Node head = new Node(null, null);
+	public boolean add(Object newObject);
 
-	public boolean add(Object newObject) {
-		Node next = head;
-		Node prev = next;
-		prev.lock.lock();
-		while (next != null) {
-			next.lock.lock();
-			prev.lock.unlock();
-			prev = next;
-		}
-		prev.next = new Node(newObject, null);
-		prev.lock.unlock();
-		return true;
-	}
+	public boolean remove(Object objectToRemove);
 
-	public boolean remove(Object objectToRemove) {
-		Node next = head;
-		Node prev = next;
-		prev.lock.lock();
-		while (next != null && !next.value.equals(objectToRemove)) {
-			next.lock.lock();
-			prev.lock.unlock();
-			prev = next;
-		}
-		boolean wereThisObjectInList = false;
-		if (next != null) {
-			prev.next = next.next;
-			wereThisObjectInList = true;
-		}
-		prev.lock.unlock();
-		return wereThisObjectInList;
-	}
+	public boolean contains(Object object);
 
-	public boolean contains(Object object) {
-		Node next = head;
-		Node prev = next;
-		prev.lock.lock();
-		while (next != null && !next.value.equals(object)) {
-			next.lock.lock();
-			prev.lock.unlock();
-			prev = next;
-		}
-		boolean isObjectInList = (next != null && next.value.equals(object));
-		prev.lock.unlock();
-		return isObjectInList;
-	}
 }
