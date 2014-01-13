8233627874babd09c65a7cc6016d0f9978bf69e0
unknown <student30@7-ICSR-FULL-26.pcoip.ki.agh.edu.pl>
2014-01-13 12:00:35 +0100

    poprawiony test

--- src/pl/com/morgoth/studia/semV/TW/lab9/test1.erl
+++ src/pl/com/morgoth/studia/semV/TW/lab9/test1.erl
@@ -5,7 +5,7 @@
 do() ->
     zad1:start(),
     spawn(fun() -> tester_start() end),
-	receive ok -> ok after 1000 -> ok end,
+	receive ok -> ok after 100 -> ok end,
     spawn(fun() -> tester_start() end),
     ok.
 
@@ -18,5 +18,5 @@ tester_ok()->
     io:format("~p getted resource, working....~n",[self()]),
     receive
         ok -> ok
-        after 5000 -> io:format("done ~p~n", [self()]), zad1:signal(), ok
+        after 7000 -> io:format("done ~p~n", [self()]), zad1:signal(), ok
     end.
